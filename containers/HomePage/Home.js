82ce26a1f449542c61b88b8523a93fc9504ddec7
hoseinsharifinia <hosein.sharifinia@gmail.com>
2020-05-13 19:06:16 +0430

    add image and edit header info

--- containers/HomePage/Home.js
+++ containers/HomePage/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import HomeRight from "../../components/HomePage/HomeRight";
+import HomeLeft from "../../components/HomePage/HomeLeft";
 
 const Home = (props) => {
   return (
     <div id="home" className="home-section full-with-section">
       <div className="home-section__holder">
         <HomeRight />
+        <HomeLeft />
       </div>
     </div>
   );
