f45b4720dc439eb2bb2ff43ecc4f6b79da869308
Ali Shahabi <ali_shahabi@outlook.com>
2021-02-07 19:54:52 +0330

    feat:edit styles and classes of techstack section (#22)

--- containers/HomePage/TechStack/TechItem.js
+++ containers/HomePage/TechStack/TechItem.js
@@ -2,17 +2,47 @@ import React from "react";
 
 const TechItem = ({ item }) => {
   return (
-    <div className="row skills">
-      <div className="col-9 skills__text-holder">
-        <p className="skills__text-holder--name">{item.name}</p>
+    <div className="row skillls skills ">
+      <div className="col-9 skills skills-text-holder">
+        <p className="skills skills-text-holder  skills-text-holder-name">
+          {item.name}
+        </p>
       </div>
-      <div className="col-3 skills__image-holder">
+      <div className="skills skills-image-holder col-3 ">
         <img
-          className="skills__image-holder--image"
+          className="skills skills-image-holder skills-image-holder-image"
           src={item.image}
           alt={item.name}
         />
       </div>
+      <style scoped jsx>
+        {`
+          .skills {
+            margin: 0.8rem 0;
+
+            .skills-image-holder {
+              text-align: center;
+              padding-top: 0.5em;
+              padding-bottom: 0.5em;
+              .skills-image-holder-image {
+                max-width: 60px;
+                max-height: 80px;
+              }
+            }
+            .skills-text-holder {
+              display: flex;
+              align-items: center;
+              justify-content: flex-end;
+              .skills-text-holder-name {
+                font-size: 22px;
+                line-height: 35px;
+                font-weight: 500;
+                color: #bfd1df;
+              }
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
--- containers/HomePage/TechStack/index.js
+++ containers/HomePage/TechStack/index.js
@@ -8,22 +8,47 @@ import TechItem from "./TechItem";
 const index = () => {
   return (
     <div id="tech-stack" className="tech-stack section">
-      <div className="tech-stack__wrapper section-wrapper">
+      <div className="tech-stack-wrapper section-wrapper">
         <div className="tech-stack-content content-1300">
-          <h2 className="tech-stack-content__title">تخصص ها</h2>
-          <p className="tech-stack-content__text">
+          <h2 className="tech-stack-content-title pt-4 pb-0 text-10">
+            تخصص ها
+          </h2>
+          <p className="tech-stack-content-text text-7 m-0 color-light-blue">
             بخشی از تخصص های من که در فعالیت روزانه و همچنین پروژ‌ه ها استفاده
             میکنم <FontAwesomeIcon icon={faRocket} />
           </p>
-          <div className="tech-stack-content__skills row">
+          <div className="tech-stack-content-skills  row">
             {skills.map((item, i) => (
-              <div key={i} className="col-12 col-md-6">
+              <div key={i} className="skill-item col-12 col-md-6">
                 <TechItem item={item} />
               </div>
             ))}
           </div>
         </div>
       </div>
+      <style scoped jsx>
+        {`
+          .skill-item {
+            margin-right: -100px;
+          }
+          .tech-stack {
+            .tech-stack-wrapper {
+              .tech-stack-content {
+                .tech-stack-content-skills {
+                }
+                .tech-stack-content-title {
+                  line-height: 100%;
+                  margin: 0 0 1rem;
+                  color: #0582ca;
+                }
+                .tech-stack-content-text {
+                  line-height: 35px;
+                }
+              }
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
