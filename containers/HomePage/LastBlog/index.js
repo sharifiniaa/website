fad1f174f6c4e53ed111c7d1a2ef6f7fb4b54c50
Ali Shahabi <ali_shahabi@outlook.com>
2021-02-09 14:40:17 +0330

    feat:edit last blog and resume styles (#26)

--- containers/HomePage/LastBlog/index.js
+++ containers/HomePage/LastBlog/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import BlogWrapper from '../../Blog/BlogWrapper';
+import React from "react";
+import BlogWrapper from "../../Blog/BlogWrapper";
 
 const index = () => {
-    return (
-        <div id="blog" className="last-blog section">
-            <div className='last-blog__wrapper section-wrapper'>
-                <div className='last-blog-content content-1300'>
-                    <h2 className='last-blog-content__title'>آخرین مقالات</h2>
-                    <BlogWrapper />
-                </div>
-            </div>
+  return (
+    <div id="blog" className="last-blog pt-4 bg-color-black">
+      <div className="last-blog__wrapper px-5 ">
+        <div className="last-blog-content ">
+          <h2 className="last-blog-content__title text-10 mb-5 pt-5 color-blue">
+            آخرین مقالات
+          </h2>
+          <BlogWrapper />
         </div>
-    )
+      </div>
+      <style scoped jsx>
+        {`
+          .last-blog {
+            &__wrapper {
+              position: relative;
+              .last-blog-content {
+                &__title {
+                  line-height: 100%;
+                }
+              }
+            }
+          }
+          .last-blog-content {
+            max-width: 1300px;
+          }
+        `}
+      </style>
+    </div>
+  );
 };
-
 
 export default index;
--- containers/HomePage/Resume/index.js
+++ containers/HomePage/Resume/index.js
@@ -3,13 +3,30 @@ import Timeline from "../../../components/TimeLine";
 
 const index = () => {
   return (
-    <div id="resume" className="resume section">
-      <div className="resume__wrapper section-wrapper">
-        <div className="resume-content content-1300">
-          <h2 className="resume-content__title">رزومه</h2>
+    <div id="resume" className="resume pt-4 bg-color-black">
+      <div className="resume__wrapper px-5">
+        <div className="resume-content">
+          <h2 className="resume-content__title text-10 mb-5 pt-5 color-blue">
+            رزومه
+          </h2>
           <Timeline />
         </div>
       </div>
+      <style scoped jsx>
+        {`
+          .resume {
+            position: relative;
+            &__wrapper {
+              .resume-content {
+                max-width: 1300px;
+                &__title {
+                  line-height: 100%;
+                }
+              }
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
